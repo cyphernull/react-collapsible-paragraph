3fea7bf7125dde81d6fb7e266ead17cd141bb891
cyphernull <eric.choo1997@yahoo.com>
2021-03-26 02:05:31 +0800

    Remove the now ignored directory "dist"

--- dist/utils.d.ts
+++
@@ -1,6 +0,0 @@
-import { Locale } from ".";
-export declare function getNumberFromPixel(pixel: string): number;
-export declare function compute(originText: string, maxWidth: number, maxHeight: number, lineHeight: number, locales: Locale, controlled: boolean): {
-    computed: boolean;
-    text: string;
-};
